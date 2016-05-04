e46ab314b206db58094745b9eddf86a5c7bd26ef
liaoclark <liaoclarke@gmail.com>
2016-05-05 00:06:41 +0800

    Add ConfigureServer.h defines the interface of ConfigureServer operation

--- include/Server/ConfigureServerFromFile.h
+++ include/Server/ConfigureServerFromFile.h
@@ -4,6 +4,7 @@
 #include <Server/CongfigureServer.h>
 
 #include <iostream>
+#include <fstream>
 #include <exception>
 
 namespace tvr {
@@ -14,40 +15,56 @@ namespace tvr {
 
         inline ServerPtr configureServerFromFile(std::String const &configName) {
             ServerPtr ret;
-            std::out << "" << configName << std::endl;
+            std::out << "Using config file '" << configName << "'" << std::endl;
             std::ifstream config(configName);
             if (!config.good()) {
+                std::out << "Could not open config file" << std::endl;
                 return nullptr;
             }
             tvr::server::ConfigureServer srvConfig;
-            std::out << "" << std::endl;
+            std::out << "Constructing server as configured..." << std::endl;
             try {
                 srvConfig.loadConfig(config);
                 ret = srvConfig.constructServer();
             } catch (std::exceptoin &e) {
+                std::err << "Caught exception constructing server from JSON config file: " << e.what() << std::endl;
                 return nullptr;
             }
             
-            std::out << "" << std::endl;
+            std::out << "Instantiating configured drivers..." << std::endl;
             bool success = svrConfig.instantiateDrivers();
             if (!srvConfig.getSuccessfulInstantiations().empty()) {
+                std::out << "Successes:" << std::endl;
+                for (auto const &driver : srvConfig.getSuccessfulInstantiations()) {
+                    std::out << " - " << driver << std::endl;
+                }
+                std::out << "\n";
             }
             if (!srvConfig.getFailedInstantiations().empty()) {
+                std::out << "Errors:" << std::endl;
+                for (auto const &error : srvConfig.getFailedInstantiations()) {
+                    std::out << " - " << error.first << "\t" << error.second << std::endl;
+                }
+                std::out << "\n";
             }
 
             if (srvConfig.processExternalDevices()) {
-                std::out << "" << std:endl;
+                std::out << "External devices found and parsed from config file." << std:endl;
             }
 
             if (srvConfig.processDisplay()) {
-                std::out << "" << std::endl;
+                std::out << "Display descriptor found and parsed from config file." << std::endl;
+            } else {
+                std::out << "Using Emulator HMD for display configuration. "
+                            "Did not find an alternate valid display object in config file." 
+                         << std::endl;
             }
 
             if (srvConfig.processRenderManagerParameters()) {
-                std::out << "" << std::endl;
+                std::out << "RenderManager config found and parsed from the config file." << std::endl;
             }
 
-            std::out << "" << std::endl;
+            std::out << "Triggering automatic hardware detection..." << std::endl;
             ret->triggerHardwareDetect();
 
             return ret;
